552a3a03758858ca50091f1e57632e7d0b10e24d
Lukasz Kurek <lukascoorek@gmail.com>
2019-05-16 15:42:54 +0200

    layotu & js working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "../src/styles/main.scss";
+
+const answers = ["A", "B", "B", "B"];
+const form = document.querySelector(".form");
+const resultDiv = document.querySelector(".result");
+const result = document.querySelector(".result span");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let score = 0;
+  const formAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value
+  ];
+  formAnswers.forEach((val, index) => {
+    if (val === answers[index]) {
+      score += 25;
+    }
+  });
+  scrollTo(0, 0);
+  resultDiv.classList.remove("d-none");
+  let points = 0;
+
+  const timer = setInterval(() => {
+    result.textContent = `${points}%`;
+    if (points === score) {
+      clearInterval(timer);
+    }
+    points++;
+  }, 20);
+});
